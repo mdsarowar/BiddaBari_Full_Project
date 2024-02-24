708b347e04833481233ff2f3dde4fd2171fddd5d
Mizanur-Rahman07 <mzrahman075@gmail.com>
2024-02-24 18:49:19 +0600

    Student Master & Home Page Change

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -1,7 +1,7 @@
 <header class="top-header">
     <div class="container-fluid">
         <div class="row align-items-center">
-            <div class="col-lg-4 col-md-4">
+            <div class="col-lg-4 col-md-4 mobile_res">
                 <div class="header-left">
                     <ul>
                         <li>
@@ -28,7 +28,7 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-1 col-md-4">
+            <div class="col-lg-1 col-md-4 mobile_res">
                 <div class="header-right">
                     <ul class="social-list">
                         <li>
@@ -60,27 +60,9 @@
             <div class="mobile-responsive-menu">
                 <div class="logo">
                     <a href="{{ route('front.home') }}">
-                        <img src="{{ asset(isset($siteSettings) ? $siteSettings->logo : 'frontend/assets/images/logos/logo-small.png') }}" class="logo-one" alt="logo">
-                        <img src="{{ asset(isset($siteSettings) ? $siteSettings->logo : 'frontend/assets/images/logos/logo-small-white.png') }}" class="logo-two" alt="logo">
+                                                <img src="{{ asset(isset($siteSettings) ? $siteSettings->logo : 'frontend/assets/images/logos/logo-small.png') }}" class="logo-one" alt="logo">
+                                                <img src="{{ asset(isset($siteSettings) ? $siteSettings->logo : 'frontend/assets/images/logos/logo-small-white.png') }}" class="logo-two" alt="logo">
                     </a>
-                </div>
-
-                <div class="modal" tabindex="-1">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title">Modal title</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <p>Modal body text goes here.</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -90,8 +72,8 @@
         <div class="container-fluid">
             <nav class="navbar navbar-expand-md navbar-light ">
                 <a class="navbar-brand" href="{{ route('front.home') }}">
-                        <img src="{{ asset(isset($siteSettings) ? $siteSettings->logo : 'frontend/assets/images/logos/logo.png') }}" class="logo-one" alt="Logo">
-                        <img src="{{ asset(isset($siteSettings) ? $siteSettings->logo : 'frontend/assets/images/logos/logo-2.png') }}" class="logo-two" alt="Logo">
+                                        <img src="{{ asset(isset($siteSettings) ? $siteSettings->logo : 'frontend/assets/images/logos/logo.png') }}" class="logo-one" alt="Logo">
+                                        <img src="{{ asset(isset($siteSettings) ? $siteSettings->logo : 'frontend/assets/images/logos/logo-2.png') }}" class="logo-two" alt="Logo">
                 </a>
                 <div class="nav-widget-form">
                     <form class="search-form search-form-bg" action="{{ route('search-content-home') }}" method="post">
@@ -132,15 +114,11 @@
                                 </div>
 
                             @else
-{{--                                <a href="{{ route('register') }}" class="default-btn two border-radius-50">Sign Up</a>--}}
                                 <a href="{{ route('login') }}" {{--data-bs-toggle="modal" data-bs-target="#authModal"--}} class="default-btn two border-radius-50">Sign In</a>
                             @endif
                         </div>
                     </div>
                 </div>
-
-
-
             </nav>
         </div>
     </div>
@@ -151,13 +129,11 @@
                 <div class="side-item">
                     <a href="{{ route('login') }}" class=""><i class="ri-user-2-fill"></i></a>
                 </div>
-                <div class="circle-inner">
+                <!-- <div class="circle-inner">
                     <div class="ri-search-line"></div>
-                    <!--     <div class="circle circle-two"></div>-->
-                    <!--     <div class="circle circle-three"></div>-->
-                </div>
+                </div> -->
             </div>
-            <div class="container">
+            <!-- <div class="container">
                 <div class="side-nav-inner">
                     <div class="side-nav justify-content-center align-items-center">
                         <div class="side-item">
@@ -169,12 +145,9 @@
                                 </button>
                             </form>
                         </div>
-                        <!--      <div class="side-item">-->
-                        <!--      <a href="signup.html" class="default-btn two">Sign Up</a>-->
-                        <!--     </div>-->
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </div>
